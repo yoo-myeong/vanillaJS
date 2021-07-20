11352584960648880b038f86585ff0689e11a3b5
yoo-myeong <yoou201@naver.com>
2021-07-21 01:10:42 +0900

    Update app.js

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,13 @@
 // 두가지 데이터타입 integer, string
+
+// variables를 만드는 두가지 방법 const, let
+// const는 안바뀌고 let은 바뀔수있음
+
+// 세번째 데이터타입 boolen : true, false, null
+
+// 배열 : 파이썬과 같은 리스트형식, const나 let으로 설정.
+// push, pop 가능
+
+// Objects , {}을 사용해서 생성. json형식으로 생성, value에는 함수도 들어갈 수 있음
+
+// function ~~(){} 
