145a0b7dab92443be98ca931c31578e4dae5deff
yoo-myeong <yoou201@naver.com>
2021-07-22 17:25:25 +0900

    localStorage getItem

--- 실습/app.js
+++ 실습/app.js
@@ -5,17 +5,23 @@ const hidden = "hidden"
 const username_key = "username"
 
 function loginSubmit(event) { // 관행적으로 submit인자를 event로 씀
-    event.preventDefault() // submit 이벤트가 발생할 때 JS는 현재 함수를 호출하고 이 때 JS는 event object를 argument로 준다. 나는 이때 새로고침이 되는 걸 이 코드로 막는것이다.
+    event.preventDefault() 
+    // submit 이벤트가 발생할 때 JS는 현재 함수를 호출하고 이 때 JS는 event object를 argument로 준다. 나는 이때 새로고침이 되는 걸 이 코드로 막는것이다.
     const name = loginInput.value
     localStorage.setItem(username_key, name)
     loginForm.classList.add(hidden)
-    greeting.innerText = `hello ${name}` // 변수를 string안에 넣는 방법. '이 아니고 `(느낌표키보드왼쪽)임을 유의
+    greeting.innerText = `hello, ${savedName}`
     greeting.classList.remove(hidden)
 }
 
-function handleLink(){
-    alert("clicked")
+
+const savedName = localStorage.getItem(username_key)
+
+if(savedName === null){
+    loginForm.addEventListener("submit", loginSubmit)
 }
-
-loginForm.addEventListener("submit", loginSubmit)
-
+else{
+    loginForm.classList.add(hidden)
+    greeting.innerText = `hello, ${savedName}`
+    greeting.classList.remove(hidden)
+}
