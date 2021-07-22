bfbff031548914661f86653e6a07ca5dccb04417
yoo-myeong <yoou201@naver.com>
2021-07-22 18:16:40 +0900

    clock생성

--- 실습/clock.js
+++ 실습/clock.js
@@ -1,10 +1,11 @@
-const clock = document.querySelector("#h2")
+const clock = document.querySelector("#clock")
 
 // interval이란 반복적으로 일어나야하는 것을 말함
 // 예를 들어 2초마다 일어나야 하는 것
 
-function sayHello(){
-    console.log("hello")
+function getClock(){
+    const date = new Date()
+    clock.innerText=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 }
 
-setInterval(sayHello, 5000)
+setInterval(getClock, 1000)
