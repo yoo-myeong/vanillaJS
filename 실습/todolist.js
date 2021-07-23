f630d13710588801a5bc7266cfb86811926b343f
yoo-myeong <yoou201@naver.com>
2021-07-24 00:23:52 +0900

    jason stringify와 parse

--- 실습/todolist.js
+++ 실습/todolist.js
@@ -2,11 +2,41 @@ const todoForm = document.querySelector("#todoForm")
 const todoList = document.querySelector("#todoList")
 const todoInput = document.querySelector("#todoForm input")
 
+const todos = []
+
+const Todos_key = 'todos'
+
+function saveToDos(){
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement
+    li.remove()
+}
+
+
+function paintTodo(newTodo){
+    const li = document.createElement("li")
+    const span = document.createElement("span")
+    const button = document.createElement("button")
+    span.innerText=newTodo
+    button.innerText = 'X'
+    button.addEventListener("click", deleteTodo)
+    li.appendChild(span)
+    li.appendChild(button)
+    todoList.appendChild(li)
+
+}
+
 function handleToDoSubmit(event){
     event.preventDefault()
     const newTodo = todoInput.value
-    
     todoInput.value = ""
+    todos.push(newTodo)
+    paintTodo(newTodo)
+    saveToDos()
+
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit)
