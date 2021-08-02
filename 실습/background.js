7abb63da79e20741d44d77421047aa6dfd6f0df8
yoo-myeong <yoou201@naver.com>
2021-08-03 01:01:21 +0900

    docs : 파일 이동

--- 실습/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["img1.jpg", "img2.jpg", "img3.jpg"]
-
-const todayImg = images[Math.floor(Math.random()*quote.length)]
-
-const image = document.createElement("img")
-
-image.src = `img/${todayImg}`
-
-document.body.appendChild(image)
